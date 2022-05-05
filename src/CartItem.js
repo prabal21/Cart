cea84c2baeeec62965605316e052a0279b1fc9f0
Prabal Sharma <prabalsharma0001@gmail.com>
2022-05-06 01:00:54 +0530

    binding increasing function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,36 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            title: 'Phone',
+            qty: 1,
+            price: 9999,
+        }
+    }
+    incqty = () =>{
+          console.log(this,this.state);
+    }
     render(){
+        const {title, price, qty}=this.state;
         return(
             <div className="cart-items">
                 <div className="left">
                 <img style={styles.image}></img>
                </div>
                <div className="right">
-                   <div>phone</div>
-                   <div>Rs9999</div>
-                   <div>Qty: 1</div>
+                   <div style={{fontSize:25, color:'green'}}>{this.state.title}</div>
+                   <div style={{color:'green',fontSize:25}}>Rs{price}</div>
+                   <div  style={{color:'green',fontSize:25}}>Qty: 1</div>
                    <div className="action-icon">
-                       
+                    {/*Buttons*/}
+                    <img alt='increase' 
+                    className='action-icon' 
+                    src='https://cdn-icons-png.flaticon.com/512/992/992651.png'
+                    onClick={this.incqty}/>
+                    <img alt='decrease' className='action-icon' src='https://cdn-icons.flaticon.com/png/512/2740/premium/2740679.png?token=exp=1651691449~hmac=3f3695b6933e81e140f897cd65ebf3c6'/> 
+                    <img alt='delete' className='action-icon' src='https://cdn-icons.flaticon.com/png/512/3405/premium/3405244.png?token=exp=1651691529~hmac=cf1d4f701db6eb7b8e6b3f2d0d935a06'/>   
                    </div>
                </div>
             </div>
@@ -22,9 +40,10 @@ class CartItem extends React.Component{
 
 const styles={
     image:{
-        height:110,
-        width:110,
+        height:150,
+        width:150,
         borderRadius:4,
+        backgroundColor:'grey',
     }
 }
 export default CartItem;
